9f6948b86ec9627c6fc4b792016d9def0d8381e6
KhanRizwan29 <mrrzwnkhan@gmail.com>
2026-06-04 18:41:58 +0530

    mail redirect gmail corrected

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Email</h4>
             <p>
-              <a href="mailto:example@mail.com" data-cursor="disable">
+              <a href="mailto:mrrzwnkhan@gmail.com" data-cursor="disable">
                 mrrzwnkhan@gmail.com
               </a>
             </p>
