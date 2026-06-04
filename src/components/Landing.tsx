30d62991ac18ed5a9de96b16f67068adcdc51c59
KhanRizwan29 <mrrzwnkhan@gmail.com>
2026-06-04 19:05:42 +0530

    remove the -dev text from main branch after merging it with dev

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,7 +9,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              Rizwan-dev
+              Rizwan
               <br />
               <span>Khan</span>
             </h1>
